a56c902c65e8b57db520cb05086ac8946527fb8d
EC2 Default User <ec2-user@ip-172-31-45-123.ap-northeast-1.compute.internal>
2022-10-06 09:47:03 +0000

    [Add]User処理追加

---
+++ config/routes.rb
@@ -0,0 +1,25 @@
+Rails.application.routes.draw do
+# 管理者用
+# URL /admin/sign_in ...
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+
+  namespace :admin do
+    get 'top' => 'homes#top', as: 'top'
+  end
+
+# 会員用
+# URL /users/sign_in ...
+  devise_for :users,skip: [:passwords], controllers: {
+    sessions: 'public/sessions',
+    registrations: 'public/registrations',
+  }
+
+  scope module: :public do
+    root 'homes#top'
+
+    get 'about'=>'homes#about' ,as:'about'
+  end
+
+end

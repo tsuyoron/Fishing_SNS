144d1501f38eafb1adaefcb04e78d158bb061d7e
EC2 Default User <ec2-user@ip-172-31-45-123.ap-northeast-1.compute.internal>
2022-10-20 03:26:53 +0000

    [ADD]釣果投稿機能と閲覧機能作成

--- app/controllers/public/catches_controller.rb
+++ app/controllers/public/catches_controller.rb
@@ -1,8 +1,57 @@
 class Public::CatchesController < ApplicationController
-  def index
-  end
+# before_action :ensure_correct_user, only: [:update,:edit]
 
   def show
+    @catch = Catch.find(params[:id])
+    @catch_new = Catch.new
+    # @post_comment = PostComment.new
   end
 
+  def index
+    @catch = Catch.new
+    @catches = Catch.all
+  end
+
+  def create
+    @catch = Catch.new(catch_params)
+    @catch.user_id = current_user.id
+    if @catch.save
+      redirect_to catch_path(@catch), notice: "You have created book successfully."
+    else
+      @catchs = Catch.all
+      render 'index'
+    end
+  end
+
+  def edit
+    @catch = Book.find(params[:id])
+  end
+
+  def update
+    @catch = Catch.find(params[:id])
+    if @catch.update(catch_params)
+      redirect_to catch_path(@catch), notice: "You have updated book successfully."
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @catch = Book.find(params[:id])
+    @catch.destroy
+    redirect_to catches_path
+  end
+
+  private
+
+  def catch_params
+    params.require(:catch).permit(:title,:body)
+  end
+
+  def ensure_correct_user
+    @catch = Catch.find(params[:id])
+    unless current_user == @catch.user
+      redirect_to catches_path
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :catches, dependent: :destroy
+  # has_many :favorites, dependent: :destroy
+  # has_many :post_comments, dependent: :destroy
+
+  # has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  # has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  # has_many :following_user, through: :follower, source: :followed # 自分がフォローしている人
+  # has_many :follower_user, through: :followed, source: :follower # 自分をフォローしている人
+
   has_one_attached :profile_image
+
+  validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
+  validates :introduction, length: { maximum: 50 }
 
   def self.guest
     find_or_create_by!(name: 'guestuser' ,email: 'guest@example.com') do |user|
@@ -20,5 +32,20 @@ class User < ApplicationRecord
     end
     profile_image.variant(resize_to_limit: [width, height]).processed
   end
+
+  #   # ユーザーをフォローする
+  # def follow(user_id)
+  #   follower.create(followed_id: user_id)
+  # end
+
+  # # ユーザーのフォローを外す
+  # def unfollow(user_id)
+  #   follower.find_by(followed_id: user_id).destroy
+  # end
+
+  # # フォローしていればtrueを返す
+  # def following?(user)
+  #   following_user.include?(user)
+  # end
 
 end
